e2d02e38a77aec88e55d407902d2e07f0fb0c860
leoxshn <leoxshn@gmail.com>
2020-01-20 11:15:33 +0100

    added a makefile highlighter

--- src/posidon/texter/backend/syntaxHighlighters/MakefileSyntaxHighlighter.kt
+++ src/posidon/texter/backend/syntaxHighlighters/MakefileSyntaxHighlighter.kt
@@ -4,11 +4,17 @@ import posidon.texter.Window
 import javax.swing.text.SimpleAttributeSet
 import javax.swing.text.StyleConstants
 import javax.swing.text.StyledDocument
-import kotlin.math.min
+import kotlin.math.max
 
 class MakefileSyntaxHighlighter : SyntaxHighlighter() {
 
     val variables = ArrayList<String>()
+
+    companion object {
+        val conditions = arrayOf("ifeq", "ifneq", "else", "endif", "if", "elif", "fi", "then", "for", "do", "while")
+        val operators = arrayOf("in")
+        val funcBreaks = arrayOf("continue", "done")
+    }
 
     override fun colorLine(doc: StyledDocument, lineStart: Int, line: String, lineI: Int) {
         doc.setCharacterAttributes(lineStart, line.length, defaultTextStyle(), false)
@@ -21,7 +27,7 @@ class MakefileSyntaxHighlighter : SyntaxHighlighter() {
                 if (!variable.contains(' ') && !raw.contains('\t')) {
                     variables.add(variable)
                     doc.setCharacterAttributes(lineStart, eqSignI, defaultTextStyle().apply {
-                        StyleConstants.setForeground(this, Window.theme.yellow)
+                        StyleConstants.setForeground(this, Window.theme.light_blue)
                     }, false)
                 }
             }
@@ -30,38 +36,68 @@ class MakefileSyntaxHighlighter : SyntaxHighlighter() {
                 if (line.contains(string)) {
                     val i = line.indexOf(string)
                     doc.setCharacterAttributes(lineStart + i, string.length, defaultTextStyle().apply {
-                        StyleConstants.setForeground(this, Window.theme.yellow)
+                        StyleConstants.setForeground(this, Window.theme.light_blue)
                     }, false)
                 }
             }
-        }
-        val stringSides = arrayOf("\"", "'")
-        for (stringSide in stringSides) {
-            var stringStart = line.indexOf(stringSide)
-            var stringEnd: Int
-            val stringAttrs = SimpleAttributeSet()
-            StyleConstants.setForeground(stringAttrs, Window.theme.green)
-            while (line.substring(stringStart + 1).contains(stringSide)) {
-                stringEnd = line.indexOf(stringSide, stringStart + 1)
+
+            var startPos = lineStart
+            val str = StringBuilder()
+            for (i in 0..line.length) {
+                if (i == line.length || " \t".contains(line[i])) {
+                    when (str.toString()) {
+                        in conditions -> doc.setCharacterAttributes(
+                            startPos,
+                            max(str.length, 1),
+                            defaultTextStyle().apply {
+                                StyleConstants.setForeground(this, Window.theme.orange)
+                            }, false)
+                        in operators -> doc.setCharacterAttributes(
+                            startPos,
+                            max(str.length, 1),
+                            defaultTextStyle().apply {
+                                StyleConstants.setForeground(this, Window.theme.cyan)
+                            }, false)
+                        in funcBreaks -> doc.setCharacterAttributes(
+                            startPos,
+                            max(str.length, 1),
+                            defaultTextStyle().apply {
+                                StyleConstants.setForeground(this, Window.theme.orange)
+                            }, false)
+                    }
+                    startPos += str.length + 1
+                    str.clear()
+                } else str.append(line[i])
+            }
+
+            val stringSides = arrayOf("\"", "'")
+            for (stringSide in stringSides) {
+                var stringStart = line.indexOf(stringSide)
+                var stringEnd: Int
+                val stringAttrs = SimpleAttributeSet()
+                StyleConstants.setForeground(stringAttrs, Window.theme.green)
+                while (line.substring(stringStart + 1).contains(stringSide)) {
+                    stringEnd = line.indexOf(stringSide, stringStart + 1)
+                    doc.setCharacterAttributes(
+                        lineStart + stringStart,
+                        stringEnd - stringStart + 1,
+                        stringAttrs,
+                        false
+                    )
+                    if (line.substring(stringEnd + 1).contains(stringSide))
+                        stringStart = line.indexOf(stringSide, stringEnd + 1)
+                    else break
+                }
+            }
+            val lineCommentI = line.indexOf('#')
+            if (lineCommentI != -1) {
                 doc.setCharacterAttributes(
-                    lineStart + stringStart,
-                    stringEnd - stringStart + 1,
-                    stringAttrs,
+                    lineStart + lineCommentI,
+                    line.length - lineCommentI,
+                    defaultTextStyle().apply { StyleConstants.setForeground(this, Window.theme.gray) },
                     false
                 )
-                if (line.substring(stringEnd + 1).contains(stringSide))
-                    stringStart = line.indexOf(stringSide, stringEnd + 1)
-                else break
             }
-        }
-        val lineCommentI = line.indexOf('#')
-        if (lineCommentI != -1) {
-            doc.setCharacterAttributes(
-                lineStart + lineCommentI,
-                line.length - lineCommentI,
-                defaultTextStyle().apply { StyleConstants.setForeground(this, Window.theme.gray) },
-                false
-            )
         }
     }
 }
